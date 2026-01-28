5000444fc4e2b664fb2396dcb6a4d2658bfd7414
TrishonParrilla <trishonp1@gmail.com>
2026-01-27 20:02:00 -0700

    Javascript variable practice

---
+++ scripts/assignment1.js
@@ -0,0 +1,31 @@
+let mainCharacter = "Shiba";
+let verb = "barked";
+let place = "at the sunset";
+let reason = "for nothing";
+let secondCharacter = "Jason";
+let thirdCharacter = "bird";
+let thirdCharacterAction = "chirping";
+
+
+const finalAction = "whilst running away";
+console.log("---funny story----");
+console.log(`The ${mainCharacter} ${verb} ${place}.`);
+console.log(`${secondCharacter} couldn't figure why they were making all that noise ${reason}. `);
+console.log(`Turns out there was a ${thirdCharacter} that was ${thirdCharacterAction} just beyond the horizon.`);
+
+
+// diff variables now :D
+mainCharacter = "owl";
+verb = "hooted";
+place = "in the direction of the moon";
+reason = "without purpose";
+secondCharacter = "shiba";
+thirdCharacter = "mouse";
+
+console.log("---funny story 2.0----")
+console.log(`The ${mainCharacter} ${verb} ${place}.`);
+console.log(`The ${secondCharacter} couldn't figure why they were making all that noise ${reason}. `);
+console.log(`Turns out there was a ${thirdCharacter} that was ${thirdCharacterAction} just beyond the horizon.`);
+
+
+
